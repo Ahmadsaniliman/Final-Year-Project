9b903e6f0571e1e2037f0dba9fcdab48933e68bd
Ahmadsaniliman <saniahmadliman@gmail.com>
2023-08-08 23:06:37 +0100

    work on

--- lib/main.dart
+++ lib/main.dart
@@ -44,3 +44,12 @@ class MyWidget extends StatelessWidget {
     return const Placeholder();
   }
 }
+
+class MyWidget1 extends StatelessWidget {
+  const MyWidget1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
